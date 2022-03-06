2221fd2f6b48f7bd3eebba60b56500f97f93bbb3
Ian Schoenrock <63610950+ischoenrock@users.noreply.github.com>
2022-03-06 14:34:41 -0700

    Initial Commit

---
+++ NewsAPITest/NewsAPITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPITestApp.swift
+//  NewsAPITest
+//
+//  Created by ian schoenrock on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
